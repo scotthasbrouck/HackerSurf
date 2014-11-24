74b51f254d337c3408fde3f2b67e86a253e9bca7
Scott Hasbrouck <scott@ginkgotree.com>
2014-11-24 11:32:15 -0500

    Refactors scraping

--- server/HackerSurf.js
+++ server/HackerSurf.js
@@ -3,51 +3,35 @@ interval = 300000; // 5 mins
 
  function startScrape() {
 	for (var i = 0; i < Meteor.scrapes.length; i++) {
-		var thisjob = Object.create(Meteor.scrapes[i]);
-		scrapeMethod(thisjob);
+		var jobsite = Object.create(Meteor.scrapes[i]);
+		scrapeMethod(jobsite);
 	}
 };
 
-function scrapeMethod(thisjob) {
-	result = Meteor.http.get(thisjob.url, {timeout:30000});
+function scrapeMethod(jobsite) {
+	result = Meteor.http.get(jobsite.url, {timeout:30000});
 	$ = cheerio.load(result.content);
-	
-	var titles = [];
-	var engagements = [];
-	var locations = [];
-	var links = [];
+
 	var jobs = [];
 
-	$(thisjob['titles']).each(function() {
-		titles.push($(this).text());
-	});
-	$(thisjob['engagements']).each(function() {
-		engagements.push($(this).text());
-	});
-	$(thisjob['locations']).each(function() {
-		locations.push($(this).text());
-	});
-	$(thisjob['links']).each(function() {
-		if($(this).attr('href').substring(0,4) === 'http') {
-			links.push($(this).attr('href'));
-		} else {
-			links.push(thisjob['linkprefix'] + $(this).attr('href'));
-		}
-	});
-
-	for(var i = 0; i < titles.length; i++) {
+	$(jobsite['container']).each(function() {
+		var href = $(this).find(jobsite['links']).eq(0).attr('href');
 		jobs.push({
-			title: titles[i],
-			engagement: engagements[i],
-			location: locations[i],
-			link: links[i]
+			title: $(this).find(jobsite['titles']).eq(0).text() || '',
+			engagement: $(this).find(jobsite['engagements']).eq(0).text() || '',
+			location: $(this).find(jobsite['locations']).eq(0).text() || '',
+			company: $(this).find(jobsite['companies']).eq(0).text() || '',
+			link: ((href.substring(0,4) === 'http') ? href : jobsite['linkprefix'] + href) || ''
 		});
-	}
+	});
+	saveJobs(jobsite, jobs);
+}
 
-	if(Scrapes.find({ url:thisjob.url }).count()) {
-		console.log('update ' + thisjob.url);
+function saveJobs(jobsite, jobs) {
+	if(Scrapes.find({ url:jobsite.url }).count()) {
+		console.log('update ' + jobsite.url);
 		Scrapes.update({
-			url:thisjob.url 
+			url:jobsite.url 
 		}, {
 			$set: {
 				updatedAt: new Date(),
@@ -55,12 +39,12 @@ function scrapeMethod(thisjob) {
 			}
 		});
 	} else {
-		console.log('insert ' + thisjob.url);
+		console.log('insert ' + jobsite.url);
 		Scrapes.insert({
-			url: thisjob.url,
-			sitename: thisjob.sitename,
+			url: jobsite.url,
+			sitename: jobsite.sitename,
 			createdAt: new Date(),
-			icon: thisjob.icon,
+			icon: jobsite.icon,
 			jobs: jobs
 		});
 	}

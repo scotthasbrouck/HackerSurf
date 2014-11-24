741a2da96e3ebd537fe81bd3f51b6583a0b4b432
Scott Hasbrouck <scott@ginkgotree.com>
2014-11-24 10:22:50 -0500

    Scrapes all sites every 5 minutes

--- server/HackerSurf.js
+++ server/HackerSurf.js
@@ -1,74 +1,72 @@
 cheerio = Meteor.npmRequire('cheerio');
+interval = 300000; // 5 mins
 
-Meteor.startScrape = function() {
+ function startScrape() {
 	for (var i = 0; i < Meteor.scrapes.length; i++) {
-		Meteor.scrapeJobs(Meteor.scrapes[i]);
+		var thisjob = Object.create(Meteor.scrapes[i]);
+		scrapeMethod(thisjob);
 	}
 };
 
-Meteor.scrapeJobs = function(jobsite) {
-	var thisjob = Object.create(jobsite);
-	//setInterval(function(){
-		result = Meteor.http.get(thisjob.url, {timeout:30000});
-		$ = cheerio.load(result.content);
-		
-		var titles = [];
-		var engagements = [];
-		var locations = [];
-		var links = [];
-		var jobs = [];
+function scrapeMethod(thisjob) {
+	result = Meteor.http.get(thisjob.url, {timeout:30000});
+	$ = cheerio.load(result.content);
+	
+	var titles = [];
+	var engagements = [];
+	var locations = [];
+	var links = [];
+	var jobs = [];
 
-		$(thisjob['titles']).each(function() {
-			titles.push($(this).text());
+	$(thisjob['titles']).each(function() {
+		titles.push($(this).text());
+	});
+	$(thisjob['engagements']).each(function() {
+		engagements.push($(this).text());
+	});
+	$(thisjob['locations']).each(function() {
+		locations.push($(this).text());
+	});
+	$(thisjob['links']).each(function() {
+		if($(this).attr('href').substring(0,4) === 'http') {
+			links.push($(this).attr('href'));
+		} else {
+			links.push(thisjob['linkprefix'] + $(this).attr('href'));
+		}
+	});
+
+	for(var i = 0; i < titles.length; i++) {
+		jobs.push({
+			title: titles[i],
+			engagement: engagements[i],
+			location: locations[i],
+			link: links[i]
 		});
-		$(thisjob['engagements']).each(function() {
-			engagements.push($(this).text());
-		});
-		$(thisjob['locations']).each(function() {
-			locations.push($(this).text());
-		});
-		$(thisjob['links']).each(function() {
-			if($(this).attr('href').substring(0,4) === 'http') {
-				links.push($(this).attr('href'));
-			} else {
-				links.push(jobsite['linkprefix'] + $(this).attr('href'));
+	}
+
+	if(Scrapes.find({ url:thisjob.url }).count()) {
+		console.log('update ' + thisjob.url);
+		Scrapes.update({
+			url:thisjob.url 
+		}, {
+			$set: {
+				updatedAt: new Date(),
+				jobs: jobs
 			}
 		});
-
-
-		for(var i = 0; i < titles.length; i++) {
-			jobs.push({
-				title: titles[i],
-				engagement: engagements[i],
-				location: locations[i],
-				link: links[i]
-			});
-		}
-
-		if(Scrapes.find({ url:thisjob.url }).count()) {
-			console.log('update ' + thisjob.url);
-			Scrapes.update({
-				url:thisjob.url 
-			}, {
-				$set: {
-					updatedAt: new Date(),
-					icon: thisjob.icon,
-					jobs: jobs
-				}
-			});
-		} else {
-			console.log('insert ' + thisjob.url);
-			Scrapes.insert({
-				url: thisjob.url,
-				sitename: thisjob.sitename,
-				createdAt: new Date(),
-				icon: thisjob.icon,
-				jobs: jobs
-			});
-		}
-	//}, thisjob.timeout);
-};
+	} else {
+		console.log('insert ' + thisjob.url);
+		Scrapes.insert({
+			url: thisjob.url,
+			sitename: thisjob.sitename,
+			createdAt: new Date(),
+			icon: thisjob.icon,
+			jobs: jobs
+		});
+	}
+}
 
 Meteor.startup(function () {
-  	Meteor.startScrape();
+	startScrape(); //call once to initiate
+  	Meteor.setInterval(startScrape, interval); //then every internal
  });

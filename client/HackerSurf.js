9ba259124e7aeea768e144ab0db77aa4c408e22e
Ronnie Miller <ronniemiller@gmail.com>
2014-12-03 22:26:48 -0800

    Adds persistence to job site filters

--- client/HackerSurf.js
+++ client/HackerSurf.js
@@ -3,31 +3,39 @@
 // http://scotthasbrouck.com
 // Client Code
 
-//db call to get jobs and jobsites
+Meteor.startup(function() {
+	if (Session.get("checkedSitesCount") === undefined) {
+		Session.set("allSites", true);
+	}
+});
+
+// db call to get jobs and jobsites
 var filteredJobSites = function(setSession, returnCount, returnSiteCount, setAllChecked, setAllUnchecked) {
 	var jobsites = Scrapes.find({}, { url: 1, sitename: 1, _id: 0, sort: {sitename: 1} }).fetch();
 	var jobscount = 0;
 	var checkedJobSites = 0;
 	jobsites.forEach(function(jobsite) {
-		jobsite.checked = ((setAllChecked) ? true : ((setAllUnchecked) ? false : ((Session.get("activeSites." + jobsite.url) === undefined) ? true : Session.get("activeSites." + jobsite.url))));
-		if(setSession) {
-			Session.set("activeSites." + jobsite.url, jobsite.checked);
-		}
+		siteActive = Session.get("activeSites." + jobsite.url);
+		jobsite.checked = (setAllChecked || Session.get("allSites")) ? true : (setAllUnchecked ? !setAllUnchecked : (siteActive !== undefined ? siteActive : false));
 		if (jobsite.checked) {
 			checkedJobSites++;
 			if (returnCount) {
 				jobscount += jobsite.jobscount;
 			}
 		}
+		if (setSession) {
+			Session.setPersistent("activeSites." + jobsite.url, jobsite.checked);
+			Session.setPersistent("checkedSitesCount", checkedJobSites);
+		}
 	});
 	return (returnCount) ? jobscount : (returnSiteCount) ? checkedJobSites : jobsites;
 };
 
-//helpers
+// Helpers
 Template.body.helpers({
 	jobs: filteredJobSites,
 	allSitesSession: function() {
-		return ((Session.get("allSites") === undefined) ? true : Session.get("allSites"));
+		return (Session.get("allSites") === undefined) ? false : Session.get("allSites");
 	},
 	jobscount: function() {
 		return filteredJobSites(false, true);
@@ -43,23 +51,23 @@ Template.body.helpers({
 //events
 Template.body.events({
 	"change .all-sites-check input": function (event) {
-    	Session.set("allSites", event.target.checked);
-    	if (event.target.checked) {
-    		filteredJobSites(true, false, false, true);
-    	} else {
-			filteredJobSites(true, false, false, false, true);
-    	}
-    }
+			Session.set("allSites", event.target.checked);
+			if (event.target.checked) {
+				filteredJobSites(true, false, false, true);
+			} else {
+				filteredJobSites(true, false, false, false, true);
+			}
+		}
 });
 
 Template.jobsite.events({
 	"change input": function (event) {
-		Session.set("activeSites." + event.target.name, event.target.checked);
-		if(!event.target.checked) {
+		Session.setPersistent("activeSites." + event.target.name, event.target.checked);
+		if (!event.target.checked) {
 			Session.set("allSites", false);
 		}
-    }
+	}
 });
 
-//Subscribe
+// Subscribe
 Meteor.subscribe("scrapes");
